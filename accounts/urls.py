9902e5fcc958277131308320fe43bd41eb3997ef
Shraddha-Hinge <154316531+Shraddha-Hinge@users.noreply.github.com>
2025-02-28 21:26:44 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView, VerifyEmailView, LoginView, ProfileView
+from .views import RegisterView, VerifyEmailView, LoginView, ProfileView, GoogleLoginView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('verify-email/<str:token>/', VerifyEmailView.as_view(), name='verify-email'),
     path('login/', LoginView.as_view(), name='login'),
+    path('google-login/', GoogleLoginView.as_view(), name='google-login'),  
     path('profile/', ProfileView.as_view(), name='profile'),
 ]
